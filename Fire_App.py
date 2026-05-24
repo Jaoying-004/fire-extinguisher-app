0bae627d3fae90061d877522817c8a864dcc3506
Jaoying-004 <seekoolatchariya@gmail.com>
2026-05-24 14:26:57 +0700

    Add files via upload

--- Fire_App.py
+++ Fire_App.py
@@ -694,7 +694,7 @@ with tab1:
                 df_display.style.set_properties(**{
                     'background-color': '#cbd8f2',  # บังคับพื้นหลังในตารางให้เป็นสีน้ำเงินเข้มตามธีม
                     'color': '#111844',  # บังคับตัวหนังสือด้านในให้เป็นสีขาวนวล (อ่านง่าย ชัดเจน 100%)
-                    'border-color': 'rgba(255, 255, 255, 0.6)' # เส้นตัดขอบในตารางจางๆ
+                    'border-color': '#FFFFFF' #เส้นตัดขอบในตารางจางๆ
                 }),
                 use_container_width=True,
                 hide_index=True
